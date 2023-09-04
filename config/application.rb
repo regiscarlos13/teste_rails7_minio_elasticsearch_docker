3664d4310131acc186a1ba724c8caf9876a5e7bd
Regis Nascimento <regis.nascimento@repassa.com.br>
2023-09-04 12:22:12 -0300

    config locales for pt_BR

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ Bundler.require(*Rails.groups)
 module TestCrudRails
   class Application < Rails::Application
     config.load_defaults 7.0
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
     config.i18n.default_locale = :'pt-BR'
     config.generators.system_tests = nil
   end
